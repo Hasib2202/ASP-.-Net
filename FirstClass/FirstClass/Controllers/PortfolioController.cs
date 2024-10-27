e2097484bfb377b9eec221e6e4fd44a5a68cd414
MD MOSTOFA HASIB <90277109+Hasib2202@users.noreply.github.com>
2024-10-27 20:23:19 +0600

    First Class
    First Class

---
+++ FirstClass/FirstClass/Controllers/PortfolioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstClass.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            return View();
+        }
+
+
+    }
+}
