391a85f559da86aa9ba834f0082edff014139484
MD MOSTOFA HASIB <90277109+Hasib2202@users.noreply.github.com>
2024-10-29 00:48:59 +0600

    2nd

---
+++ SecondClass/SecondClass/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecondClass.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        public ActionResult List()
+        {
+
+            return View();
+        }
+    }
+}
