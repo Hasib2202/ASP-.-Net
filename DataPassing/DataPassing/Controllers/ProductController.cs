f40ddb4edfadbc6d0a4eb32b9b98143d836fece9
MD MOSTOFA HASIB <90277109+Hasib2202@users.noreply.github.com>
2024-11-05 01:13:32 +0600

    Data passing
    Data passing

---
+++ DataPassing/DataPassing/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        public ActionResult List()
+        {
+
+            return View();
+        }
+    }
+}
